553aa74fd53433f5ce14c4adbbc99d47c76be4d3
eivindveg <eivindveg@hotmail.com>
2014-11-03 13:30:32 +0100

    Add map

--- avanade/js/main.js
+++ avanade/js/main.js
@@ -1,1 +1,5 @@
+function start() {
+    var map = new Microsoft.Maps.Map(document.getElementById('bingMap'), {credentials: 'AiWyc3qg7x06GyClADS6IMcvlJMeD9TIeuYSf3XFlY5slfeNQUBlxj4yZMIxb1rp'});
 
+    
+}
