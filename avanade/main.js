b04e9fdcd8db2f686da3f1a643a30b166a43948e
eivindveg <eivindveg@hotmail.com>
2014-11-03 14:24:08 +0100

    Add dropoffpoint

--- avanade/main.js
+++ avanade/main.js
@@ -1,4 +1,3 @@
-
 var positions;
 var vehicles = [];
 var assignments;
@@ -10,14 +9,13 @@ function onLoad() {
 }
 
 
-function GetMap()
-{   
+function GetMap() {
 
-    map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
-				     {credentials: "ArN_71lhhNDJN7Nc0zaNI5wX2zFgqkErgfZJTLXNWU78-WYpMD6CK9a2Q25RUpCo",
-				      center: new Microsoft.Maps.Location(45.5, -122.5),
-				      mapTypeId: Microsoft.Maps.MapTypeId.road,
-				      zoom: 7});
+    map = new Microsoft.Maps.Map(document.getElementById("mapDiv"),
+        {credentials: "ArN_71lhhNDJN7Nc0zaNI5wX2zFgqkErgfZJTLXNWU78-WYpMD6CK9a2Q25RUpCo",
+            center: new Microsoft.Maps.Location(45.5, -122.5),
+            mapTypeId: Microsoft.Maps.MapTypeId.road,
+            zoom: 7});
 }
 
 
@@ -27,60 +25,76 @@ function sortByProperty(array, propertyName) {
     });
 }
 
-function addVehicleToMap(d)
-{
+function addVehicleToMap(d) {
     console.log(d);
     var b = new Microsoft.Maps.Location(d.latitude, d.longitude);
     var pin = new Microsoft.Maps.Pushpin(
-	b, {text: d.vehicleId}
+        b, {text: d.vehicleId}
     );
     map.entities.push(pin);
     map.setView({center: b})
 }
 
-function addVehiclePath(id)
-{
+function addVehiclePath(id) {
     var arr = [];
-    for(var i=0; i < vehicles[id].length; ++i) {
-	arr[i] = new Microsoft.Maps.Location(vehicles[id][i].latitude,
-					     vehicles[id][i].longitude);
+    for (var i = 0; i < vehicles[id].length; ++i) {
+        arr[i] = new Microsoft.Maps.Location(vehicles[id][i].latitude,
+            vehicles[id][i].longitude);
     }
-    
+
     var l = new Microsoft.Maps.Polyline(arr);
     map.entities.push(l);
 }
 
 
-
-function populateMap () {
+function populateMap() {
     $.getJSON("http://novatransport.azurewebsites.net/api/vehiclelocations",
-	      {apiKey:"a59c8fc125ca463a85433e6ef01190fe",
-   	      },
-	      function(data) {
-		  positions = data;
+        {apiKey: "a59c8fc125ca463a85433e6ef01190fe"
+        },
+        function (data) {
+            positions = data;
 
-		  //sortByProperty(data, "time");
-
-
-
-		  for(var i=0; i < positions.length; ++i) {
-		      id = positions[i].vehicleId; 
-		      if(!vehicles[id])
-			  vehicles[id] = []
-		      vehicles [id].push(positions[i]);
-		  }
-
-		  for(var i=0; i < vehicles.length; ++i)
-		      if(vehicles[i]) {
-			  addVehicleToMap(vehicles[i][0]);
-		      }
-
-	      });
-    
-
-    
-    
+            //sortByProperty(data, "time");
 
 
+            for (var i = 0; i < positions.length; ++i) {
+                id = positions[i].vehicleId;
+                if (!vehicles[id])
+                    vehicles[id] = []
+                vehicles [id].push(positions[i]);
+            }
+
+            for (var i = 0; i < vehicles.length; ++i)
+                if (vehicles[i]) {
+                    addVehicleToMap(vehicles[i][0]);
+                }
+
+        });
+
+
+}
+
+function DropOffPoint() {
+    this.id = 0;
+    this.latitude = 0;
+    this.longitude = 0;
+    this.description = 0;
+
+    function getPointById(id) {
+        var latitude;
+        var longitude;
+        $.getJSON("http://novatransport.azurewebsites.net/api/droppoints/" + id,
+            {apiKey: "a59c8fc125ca463a85433e6ef01190fe"
+            },
+            function (data) {
+                latitude = data['latitude'];
+                longitude = data['longitude'];
+            });
+        var point = new DropOffPoint();
+        point.latitude = latitude;
+        point.longitude = longitude;
+        point.id = id;
+        return point;
+    }
 }
 
