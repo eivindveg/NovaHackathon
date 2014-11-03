af5bda14e398347adba78dc796b0602cc320b852
eivindveg <eivindveg@hotmail.com>
2014-11-03 14:39:42 +0100

    Add dropoffpoints to map

--- avanade/js/main.js
+++ avanade/js/main.js
@@ -1,5 +1,23 @@
+var baseUrl = "http://novatransport.azurewebsites.net/api/";
+var apiKey = "a59c8fc125ca463a85433e6ef01190fe";
+var vehicles;
+
+function getAllVehicles() {
+    var data = {};
+    data['apiKey'] = apiKey;
+    jQuery.get(baseUrl + "vehicles", data, function(jqXHR) {
+        vehicles = data;
+    });
+}
 function start() {
     var map = new Microsoft.Maps.Map(document.getElementById('bingMap'), {credentials: 'AiWyc3qg7x06GyClADS6IMcvlJMeD9TIeuYSf3XFlY5slfeNQUBlxj4yZMIxb1rp'});
 
-    
+    var vehicles = getAllVehicles();
+}
+
+
+function start() {
+    var map = new Microsoft.Maps.Map(document.getElementById('bingMap'), {credentials: 'AiWyc3qg7x06GyClADS6IMcvlJMeD9TIeuYSf3XFlY5slfeNQUBlxj4yZMIxb1rp'});
+
+
 }
